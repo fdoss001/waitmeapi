cf590d0d2d6671d8d1c409cbaf1c08f00a6cc0c4
FERNANDO-MSI\Fernando <fdoss001@gmail.com>
2021-05-20 14:58:16 -0400

    fix for server

--- src/main/java/com/waitme/WaitmeApplication.java
+++ src/main/java/com/waitme/WaitmeApplication.java
@@ -2,11 +2,17 @@ package com.waitme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WaitmeApplication extends SpringBootServletInitializer {
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WaitmeApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WaitmeApplication.class, args);
 	}
